922c148bf51d0ce5ab27261b40544a27ce7c2e8f
AKengg1 <ak72532961@gmail.com>
2026-05-29 09:46:46 +0530

    added css

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-const [catImage, setCatImage] = useState<string>('')
-const [name, setName] = useState<string>('')
-const [counter, setCounter] = useState<number>(0)
+  const [counter, setCounter] = useState<number>(0);
+  const [data, setData] = useState<any>(null);
 
   useEffect(() => {
-fetch('http://api.freeapi.app/api/v1/public/cats/cat/random')
-      .then(response => response.json())
-      .then(data => {
-        setCatImage(data.data.image)
-        setName(data.data.name)
-        // Handle the API response
+    fetch("http://api.freeapi.app/api/v1/public/cats/cat/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data.data);
       })
-      .catch(error => {
-        console.error('Error fetching cat image:', error)
-      })
-  }, [counter])
+      .catch((error) => {
+        console.error("Error fetching cat image:", error);
+      });
+  }, [counter]);
 
   return (
-    <>
+    <div className="card">
       <h1>Random Cat Image</h1>
-      <p>{name}</p>
-      <img src={catImage} alt="" />
+      <article>
+        <h3>{data?.name}, </h3>
+        {data?.temperament}
+      </article>
+      <h6>ORIGIN: {data?.origin}</h6>
+      <p>{data?.description}</p>
+
+      <a href={data?.wikipedia_url} target="_blank" rel="noopener noreferrer">
+        Learn more about this cat
+      </a>
+      <img src={data?.image} alt="" />
       <button onClick={() => setCounter(counter + 1)}>Get New Cat Image</button>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
